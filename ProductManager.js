8efa83e728955565b3c3f764bd6a7bf71f482297
Nicolas Vaglienti <nicolasvaglienti1@gmail.com>
2022-12-28 20:31:20 -0300

    Primera-pre-entrega

--- ProductManager.js
+++ ProductManager.js
@@ -1,12 +1,5 @@
 import fs from "fs";
-
-const writeFile = async (path, contents) =>
-  await fs.promises.writeFile(path, JSON.stringify(contents, null, 2));
-
-const readFile = async (path) => {
-  const data = await fs.promises.readFile(path, "utf8");
-  return JSON.parse(data);
-};
+import { writeFile, readFile } from "./helpers.js";
 
 class ProductManager {
   constructor(filePath) {
@@ -19,39 +12,47 @@ class ProductManager {
     }
   }
 
-  async addProduct(title, description, price, thumbnail, code, stock) {
-    try {
-      if (this.products.find((p) => p.code === code)) {
-        return console.log("Error: Codigo ya existe.");
-      } else if (!title) {
-        return console.log("Error: titulo es requerido.");
-      } else if (!description) {
-        return console.log("Error: descripción es requerida.");
-      } else if (!price) {
-        return console.log("Error: precio es requerido.");
-      } else if (!thumbnail) {
-        return console.log("Error: thumbnail es requerido.");
-      } else if (!code) {
-        return console.log("Error: codigo es requerido.");
-      } else if (!stock) {
-        return console.log("Error: stock es requerido.");
-      }
-
-      let product = {
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-        id: this.products.length + 1,
-      };
-
-      this.products.push(product);
-      await writeFile(this.path, this.products);
-    } catch (error) {
-      console.log("Error linea 49", error);
+  async addProduct(product) {
+    const {
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+      category,
+      status,
+    } = product;
+    if (this.products.find((p) => p.code === code)) {
+      throw new Error("Error: Codigo ya existe.");
+    } else if (!title) {
+      throw new Error("Error: titulo es requerido.");
+    } else if (!description) {
+      throw new Error("Error: descripción es requerida.");
+    } else if (!price) {
+      throw new Error("Error: precio es requerido.");
+    } else if (!thumbnail) {
+      throw new Error("Error: thumbnail es requerido.");
+    } else if (!code) {
+      throw new Error("Error: codigo es requerido.");
+    } else if (!stock) {
+      throw new Error("Error: stock es requerido.");
     }
+
+    let p = {
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+      category,
+      status,
+      id: this.products.length + 1,
+    };
+
+    this.products.push(p);
+    await writeFile(this.path, this.products);
   }
 
   getProducts({ limit }) {
@@ -61,21 +62,24 @@ class ProductManager {
     return this.products;
   }
 
-  async updateProduct(product) {
-    try {
-      this.products = this.products.map((p) => {
-        if (p.id === product.id) {
-          return {
-            ...p,
-            ...product,
-          };
-        }
-        return p;
-      });
-      await writeFile(this.path, this.products);
-    } catch (error) {
-      console.log(error);
+  async updateProduct(product, id) {
+    if (product.hasOwnProperty("id")) {
+      throw new Error("Error: El id no puede ser modificado.");
     }
+    const p = this.products.find((p) => id === p.id);
+    if (!p) {
+      throw new Error("Error: El producto no existe.");
+    }
+    this.products = this.products.map((p) => {
+      if (p.id === id) {
+        return {
+          ...p,
+          ...product,
+        };
+      }
+      return p;
+    });
+    await writeFile(this.path, this.products);
   }
 
   async deleteProduct(id) {
@@ -86,7 +90,6 @@ class ProductManager {
   async getProductsById(id) {
     try {
       this.products = await readFile(this.path);
-      console.log(this.products);
       const product = this.products.find((p) => p.id === id);
       if (!product) {
         return console.log("Error: Not found");
@@ -97,35 +100,5 @@ class ProductManager {
     }
   }
 }
-
-// let minm = 1;
-// let maxm = 100;
-
-// for (let i = 0; i < 5; i++) {
-//   productManager.addProduct(
-//     "Product",
-//     "decription",
-//     100,
-//     "thumbnail",
-//     Math.floor(Math.random() * (maxm - minm + 1)) + minm,
-//     15
-//   );
-// }
-
-// productManager.updateProduct({
-//   title: "Product",
-//   description: "nueva asdasasddescripcion",
-//   price: 100,
-//   thumbnail: "thumbnail",
-//   code: 6,
-//   stock: 15,
-//   id: 3,
-// });
-
-// Descomentar los logs si es necesario.
-
-// console.log(productManager.getProducts());
-// console.log(productManager.getProductsById(1));
-// productManager.deleteProduct(2);
 
 export default new ProductManager("./Products.json");
--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-import express from "express";
-import productManager from "./ProductManager.js";
+import express, { urlencoded } from "express";
+import ProductRouter from "./routes/products.js";
+import CartRouter from "./routes/cart.js";
 
 const app = express();
+const port = 8080;
 
-app.get("/products", (req, res) => {
-  const limit = req.query.limit;
-  const products = productManager.getProducts({ limit });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  console.log(products);
+app.use("/api/products", ProductRouter);
+app.use("/api/cart", CartRouter);
 
-  if (!products) {
-    res.send("hubo un error");
-  }
-
-  res.json(products);
-});
-
-app.get("/products/:id", async (req, res) => {
-  const params = req.params;
-  console.log(params);
-  const product = await productManager.getProductsById(Number(params.id));
-  console.log(product);
-  res.json(product);
-});
-
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(port, () => console.log(`Listening on port ${port}`));
